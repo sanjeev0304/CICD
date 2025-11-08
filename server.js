518812651fd68b2b4715a070b4f4aa9cfcb685c5
sanjeev-0304 <sanjeev.modukuri@lucidact.com>
2025-11-08 14:44:12 +0530

    demonstrate changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from ExpressJS" });
+  res.json({ message: "Hello from CICD EXPO, We are students form KLH studying in 3rd year CSIT" });
 });
 
 app.listen(3000, "0.0.0.0", () => console.log("Server running on port 3000"));
