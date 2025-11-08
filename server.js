c21364bff03c68f5fc002a4bedec34859efbe6c6
sanjeev-0304 <sanjeev.modukuri@lucidact.com>
2025-11-08 14:21:43 +0530

    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from Dockerized Node.js on EC2!" });
+  res.json({ message: "Hello from ExpressJS" });
 });
 
 app.listen(3000, "0.0.0.0", () => console.log("Server running on port 3000"));
